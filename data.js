14e47262e3575a26922fa89c4ed7caacd01bcbf9
RomyLenoor <romyleno@student.arteveldehs.be>
2020-02-29 18:31:37 +0100

    Update data.js

--- data.js
+++ data.js
@@ -36,7 +36,7 @@ const data = {
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/091673fb-1070-45a0-69cc-b8ad0254de00-7f27/?fullscreen",
                 "target": "_blank"
             }
         ]
