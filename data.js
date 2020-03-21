a7da042aec182013fdaa94750f79c5fb87b6b65e
RomyLenoor <romyleno@student.arteveldehs.be>
2020-03-21 09:57:36 +0100

    indienen taak 2

--- data.js
+++ data.js
@@ -51,18 +51,18 @@ const data = {
             {
                 "text": "Inspiratieboek",
                 "title": "Mijn PDF met inspiratie",
-                "href": "./assignments/assignment2/documents/a2-inspiration.pdf",
+                "href": "assignments/assignment2/documents/inspiratieboek-ux-oef2.pdf",
                 "target": "_blank"
             },
             {
                 "text": "Adobe XD",
                 "title": "Mijn Adobe XD document",
-                "href": "./assignments/assignment2/documents/a2-visual-designs.xd",
+                "href": "assignments/assignment2/documents/sneaks.RomyLenoor.ux.xd",
             },
             {
                 "text": "Adobe XD Prototype",
                 "title": "Mijn Adobe XD prototype (share link)",
-                "href": "https://xd.adobe.com/view/e4c35170-a2f7-4c10-753b-ba7c449703ec-92f2/grid",
+                "href": "https://xd.adobe.com/view/e55414e3-e249-4468-5b50-17d98d475f09-9365/",
                 "target": "_blank"
             }
         ]
